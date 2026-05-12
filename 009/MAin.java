96da43a61af41f49eac20a529ac4caedc3524a73
kana-engineer <alimoovc@gmail.com>
2026-05-12 13:31:10 +0600

    Two bandits

---
+++ 009/MAin.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MAin {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt(), b = in.nextInt();
+        System.out.println((b - 1) + " " +  (a - 1));
+    }
+    
+}

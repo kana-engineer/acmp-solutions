30390fc645b8794cc114c7783db544002b21bac0
kana-engineer <alimoovc@gmail.com>
2026-05-12 13:16:02 +0600

    Enya

---
+++ 007/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
+        int result = b * c;
+        int num = result * 2;
+    System.out.println(num * a);
+    }
+}

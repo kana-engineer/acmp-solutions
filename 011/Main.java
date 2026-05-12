2d6f42735912cb3d80dc67afef4efd2d3d2a1ae2
kana-engineer <alimoovc@gmail.com>
2026-05-12 23:13:17 +0600

    Game

---
+++ 011/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println(198);
+                break;
+            case 2:
+                System.out.println(297);
+                break;
+            case 3:
+                System.out.println(396);
+                break;
+            case 4:                        
+                System.out.println(495);
+                break;
+            case 5:
+                System.out.println(594);
+                break;
+            case 6:
+                System.out.println(693);
+                break;
+            case 7:
+                System.out.println(792);
+                break;
+            case 8:
+                System.out.println(891);
+                break;
+            case 9:
+                System.out.println(990);
+        }
+    }
+}

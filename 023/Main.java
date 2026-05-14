d22628f3f5b9f7084d7aa40b4bee143ea3fc80db
kana-engineer <alimoovc@gmail.com>
2026-05-14 16:03:30 +0600

    U-turn

---
+++ 023/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+        int[] arr = new int[count];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+    
+}

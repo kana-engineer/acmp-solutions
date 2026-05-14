2193a93433702bf133a88c2184437053225f521d
kana-engineer <alimoovc@gmail.com>
2026-05-14 11:29:39 +0600

    cake

---
+++ 015/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n % 2 == 0) {
+            System.out.println(n / 2);
+        } else if(n == 1 || n == 0) {
+            System.out.println(0);
+        }
+        else {
+            System.out.println(n);
+        }
+    }
+    
+}

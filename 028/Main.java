ddcd4a1fc6eab54306c4c50d6c323797d9931516
kana-engineer <alimoovc@gmail.com>
2026-05-15 17:39:16 +0600

    Fishing net

---
+++ 028/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt(), n = in.nextInt();
+        System.out.println((m - 1) * (n -1));
+    }
+    
+}
